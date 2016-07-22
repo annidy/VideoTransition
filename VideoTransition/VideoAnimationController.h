af3da5f5cb0c1e92b4f241f8fc7a952910a92f0b
annidyfeng <annidy@gmail.com>
2016-07-22 14:36:53 +0800

    first commit

---
+++ VideoTransition/VideoAnimationController.h
@@ -0,0 +1,13 @@
+//
+//  VideoAnimationController.h
+//  VideoTransition
+//
+//  Created by annidyfeng on 16/7/21.
+//  Copyright © 2016年 annidyfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoAnimationController : NSObject
+
+@end
---
+++ VideoTransition/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  VideoTransition
+//
+//  Created by annidyfeng on 16/7/21.
+//  Copyright © 2016年 annidyfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
